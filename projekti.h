817d0e4aa8ae74f9069195b39469759c0f1379f5
johmakinen <joh.makinen@gmail.com>
2019-04-01 18:53:50 +0300

    debugging

--- projekti.h
+++ projekti.h
@@ -20,10 +20,11 @@ typedef struct course_st{
 
 }Course;
 
+int compareNum(const void* a, const void* b);
 void addStudent(char *studentId,char *name, Course *arr);
 void updatePoints(char *studentId, int round, int points, Course *arr);
-void printStudents(Course *arr);
-int compareNum(const void* a, const void* b);
+//void printStudents(Course *arr);
+//int compareNum(const void* a, const void* b);
 void printStudents(Course *arr);
 void writeToFile(char *file, Course *arr);
 void readFromFile(char *file, Course *arr);
